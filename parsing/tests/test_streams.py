efc67a3e0a90513876984a9e96bd0d558a7d8e69
David Sanders <davesque@gmail.com>
2015-10-06 11:44:12 -0600

    Move tests

---
+++ parsing/tests/test_streams.py
@@ -0,0 +1,117 @@
+from StringIO import StringIO
+import unittest
+
+from .utils import StreamError, Stream, ScrollingStream
+
+
+class TestScrollingStream(unittest.TestCase):
+    def setUp(self):
+        self.s = ScrollingStream('arst\n1234\n\n')
+
+    def test_it_should_be_rewindable(self):
+        self.assertEqual(''.join(self.s.get(4)), 'arst')
+        self.assertEqual(''.join(self.s.get(2)), '\n1')
+        self.s.unget(1)
+        self.assertEqual(''.join(self.s.get(1)), '1')
+        self.s.unget(1)
+        self.s.unget(1)
+        self.assertEqual(''.join(self.s.get(1)), '\n')
+        self.s.unget(5)
+        self.assertEqual(''.join(self.s.get(11)), 'arst\n1234\n\n')
+
+    def test_it_should_keep_track_of_its_position(self):
+        self.assertEqual(self.s.position, (1, 1))
+        self.s.get(4)
+        self.assertEqual(self.s.position, (1, 5))
+        self.s.get(2)
+        self.assertEqual(self.s.position, (2, 2))
+        self.s.unget(1)
+        self.assertEqual(self.s.position, (2, 1))
+        self.s.unget(1)
+        self.assertEqual(self.s.position, (1, 5))
+        self.s.unget(4)
+        self.assertEqual(self.s.position, (1, 1))
+        self.s.get(11)
+        self.assertEqual(self.s.position, (4, 1))
+
+    def test_it_should_raise_an_error_if_unget_passses_beginning_of_content(self):
+        with self.assertRaises(StreamError):
+            self.s.unget(1)
+
+        self.s.get(4)
+        with self.assertRaises(StreamError):
+            self.s.unget(5)
+
+    def test_if_unget_raises_an_error_should_not_jumble_content(self):
+        try:
+            self.s.unget(1)
+        except StreamError:
+            pass
+
+        self.s.get(4)
+
+        try:
+            self.s.unget(5)
+        except StreamError:
+            pass
+
+        self.s.unget(4)
+        self.assertEqual(''.join(self.s.get(8)), 'arst\n123')
+
+
+class TestStream(unittest.TestCase):
+    def setUp(self):
+        self.stream = Stream(StringIO('arst' * 2 + 'zxcv' * 2))
+
+    def test_it_should_wrap_any_object_with_a_read_method_and_provide_a_put_method(self):
+        x = self.stream.get(8)
+        self.assertEqual(u''.join(x), u'arstarst')
+
+        self.stream.put(x)
+        x = self.stream.get(12)
+        self.assertEqual(u''.join(x), u'arstarstzxcv')
+
+    def test_it_should_raise_an_exception_when_end_of_stream_reached(self):
+        x = self.stream.get(16)
+        self.assertEqual(u''.join(x), u'arstarstzxcvzxcv')
+
+        with self.assertRaises(StreamError):
+            self.stream.get(1)
+
+        self.stream.put('arst')
+
+        with self.assertRaises(StreamError):
+            self.stream.get(5)
+
+    def test_it_should_accept_a_string_as_an_argument(self):
+        s = Stream('arst')
+        self.assertEqual(''.join(s.get(4)), 'arst')
+
+    def test_it_should_return_correct_amount_of_content_from_internal_buffer_only(self):
+        x = self.stream.get(16)
+        self.stream.put(x)
+        x = self.stream.get(4)
+
+        self.assertEqual(''.join(x), 'arst')
+
+    def test_it_should_require_a_non_negative_integer(self):
+        with self.assertRaises(StreamError):
+            self.stream.get(-1)
+
+        self.assertEqual(self.stream.get(0), [])
+
+    def test_it_should_not_discard_content_read_while_eof_encountered(self):
+        try:
+            self.stream.get(17)
+        except StreamError:
+            pass
+
+        x = self.stream.get(8)
+        self.assertEqual(''.join(x), 'arstarst')
+
+    def test_it_should_not_mix_up_the_order_of_putted_content(self):
+        self.stream.put('5678')
+        self.stream.put('1234')
+
+        x = self.stream.get(8)
+        self.assertEqual(''.join(x), '12345678')
---
+++ parsing/tests/test_utils.py
@@ -0,0 +1,55 @@
+import unittest
+
+from .utils import compose, flatten, truncate, join, unary, equals
+
+
+class TestEquals(unittest.TestCase):
+    def test_it_should_return_a_function_that_compares_against_x(self):
+        self.assertTrue(equals(234)(234))
+        self.assertFalse(equals(234)(123))
+
+
+class TestUnary(unittest.TestCase):
+    def test_it_convert_a_function_into_a_unary_version_of_itself(self):
+        self.assertEqual(unary(lambda x, y: x + y)([1, 2]), 3)
+
+
+class TestJoin(unittest.TestCase):
+    def test_it_should_join_a_sequence_into_a_string(self):
+        self.assertEqual(join(list('arst')), 'arst')
+        self.assertEqual(join(map(str, [1, 2, 3, 4])), '1234')
+
+
+class TestTruncate(unittest.TestCase):
+    def test_it_should_truncate_a_string(self):
+        self.assertEqual(truncate('arst'), 'arst')
+        self.assertEqual(truncate('arstarstar'), 'arstarstar')
+        self.assertEqual(truncate('arstarstars'), 'arstarstar...')
+        self.assertEqual(truncate('arstarstarstarstarstarstarstarst'), 'arstarstar...')
+
+
+class TestCompose(unittest.TestCase):
+    def test_it_should_compose_the_given_functions(self):
+        f = compose(
+            lambda x: x + 1,
+            lambda x: x * 2,
+            lambda x: x ** 3,
+        )
+
+        self.assertEqual(f(1), 3)
+        self.assertEqual(f(2), 17)
+        self.assertEqual(f(3), 55)
+
+
+class TestFlatten(unittest.TestCase):
+    def test_it_should_flatten_an_arbitrarily_nested_list(self):
+        self.assertEqual(
+            flatten([1, 2, [3, 4, [5, 6]]]),
+            [1, 2, 3, 4, 5, 6],
+        )
+
+        heavily_nested = reduce(lambda a, i: (a, i), range(1000))
+        self.assertEqual(
+            flatten(heavily_nested),
+            list(range(1000)),
+        )
